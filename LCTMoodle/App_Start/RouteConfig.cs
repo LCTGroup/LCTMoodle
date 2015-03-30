5b6fb690adb685a010b6d059c4126cc346731016
lebinhchieu <lebinhchieu@gmail.com>
2015-03-30 12:15:35 +0700

    Xay dung layout

--- LCTMoodle/App_Start/RouteConfig.cs
+++ LCTMoodle/App_Start/RouteConfig.cs
@@ -15,8 +15,8 @@ namespace LCTMoodle
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{controller}/{action}/{ma}",
+                defaults: new { controller = "TrangChu", action = "Index", ma = UrlParameter.Optional }
             );
         }
     }
