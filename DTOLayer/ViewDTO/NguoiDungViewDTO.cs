18baf39f87b6d1c9b0863d35d22571c3d8fb5389
PenguinBlueChip <Funky_Hpio@yahoo.com>
2015-04-19 11:19:06 +0700

    nothing

---
+++ DTOLayer/ViewDTO/NguoiDungViewDTO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOLayer.ViewDTO
+{
+    class NguoiDungViewDTO : DTO
+    {
+        public class ChuDeDataDTO : DTO
+        {
+            public int ma;
+            public string tenTaiKhoan;
+            public string matKhau;
+            public string email;
+            public string hoTen;
+            public DateTime ngaySinh;
+            public string diaChi;
+            public string soDienThoai;
+
+            public override void gan(System.Data.SqlClient.SqlDataReader dong)
+            {
+                for (int i = 0; i < dong.FieldCount; i++)
+                {
+                    switch (dong.GetName(i))
+                    {
+                        case "Ma":
+                            ma = layInt(dong, i); break;
+                        case "TenTaiKhoan":
+                            tenTaiKhoan = layString(dong, i); break;
+                        case "MatKhau":
+                            matKhau = layString(dong, i); break;
+                        case "Email":
+                            email = layString(dong, i); break;
+                        case "HoTen":
+                            hoTen = layString(dong, i); break;
+                        case "NgaySinh":
+                            ngaySinh = layDateTime(dong, i, DateTime.MinValue); break;
+                        case "DiaChi":
+                            diaChi = layString(dong, i); break;
+                        case "SoDienThoai":
+                            soDienThoai = layString(dong, i); break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LCTMoodle/App_Data/NguoiDung.sql
@@ -0,0 +1,34 @@
+﻿use rtcmfraf_Moodle;
+
+GO
+--Người dùng
+CREATE TABLE dbo.NguoiDung (
+	Ma INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
+	TenTaiKhoan NVARCHAR(MAX) NOT NULL,
+	MatKhau NVARCHAR(MAX) NOT NULL,
+	Email NVARCHAR(MAX) NOT NULL,
+	HoTen NVARCHAR(MAX) NOT NULL,
+	NgaySinh DATETIME,
+	DiaChi NVARCHAR(MAX),
+	SoDienThoai NVARCHAR(MAX)
+);
+
+GO
+--Thêm người dùng
+CREATE PROC dbo.themNguoiDung (
+	@0 NVARCHAR(MAX), --Tên đăng nhập
+	@1 NVARCHAR(MAX), --Mật khẩu
+	@2 NVARCHAR(MAX), --Email
+	@3 NVARCHAR(MAX), --Họ và tên
+	@4 DATETIME, --Ngày Sinh
+	@5 NVARCHAR(MAX), --Địa chỉ
+	@6 NVARCHAR(MAX) --Số điện thoại
+)
+AS
+BEGIN
+	INSERT INTO dbo.NguoiDung(TenTaiKhoan, MatKhau, Email, HoTen, NgaySinh, DiaChi, SoDienThoai) VALUES (@0, @1, @2, @3, @4, @5, @6);
+
+	SELECT *		
+	FROM dbo.NguoiDung
+	WHERE Ma=@@IDENTITY;
+END
