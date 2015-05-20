cdb0a7b4b71fc0f0ef68d5b2e0b6000abb9a9d67
Le Binh Chieu <lebinhchieu@gmail.com>
2015-05-20 09:05:30 +0700

    code binh luan

---
+++ DTOLayer/ViewDTO/BinhLuanViewDTO.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOLayer
+{
+    public class BinhLuanViewDTO : DTO
+    {
+        public string noiDung;
+        public NguoiDungViewDTO nguoiTao;
+        public DTO doiTuong;
+        public TapTinViewDTO tapTin;
+        public DateTime? thoiDiemTao;
+
+        public override void gan(System.Data.SqlClient.SqlDataReader dong)
+        {
+            for (int i = 0; i < dong.FieldCount; i++)
+            {
+                switch (dong.GetName(i))
+                {
+                    case "Ma":
+                        ma = layInt(dong, i);
+                        break;
+                    case "NoiDung":
+                        noiDung = layString(dong, i);
+                        break;
+                    case "MaNguoiTao":
+                        nguoiTao = new NguoiDungViewDTO() {
+                            ma = layInt(dong, i)
+                        };
+                        break;
+                    case "MaDoiTuong":
+                        doiTuong = new DTO() {
+                            ma = layInt(dong, i)
+                        };
+                        break;
+                    case "MaTapTin":
+                        tapTin = new TapTinViewDTO() {
+                            ma = layInt(dong, i)
+                        };
+                        break;
+                    case "ThoiDiemTao":
+                        thoiDiemTao = layDateTime(dong, i);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
