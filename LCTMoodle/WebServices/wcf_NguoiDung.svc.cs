fb759fdb1130b438f7c45cc0a08c8726ad22b2d7
csphong94 <ng.huyphong94@gmail.com>
2015-06-03 14:14:46 +0700

    Starting webservice

---
+++ LCTMoodle/WebServices/wcf_NguoiDung.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace LCTMoodle.WebServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "wcf_NguoiDung" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select wcf_NguoiDung.svc or wcf_NguoiDung.svc.cs at the Solution Explorer and start debugging.
+    public class wcf_NguoiDung : Iwcf_NguoiDung
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
