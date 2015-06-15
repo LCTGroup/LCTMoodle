cef2066b731fda0489f59202a9f44a1fb29ceca7
Le Binh Chieu <lebinhchieu@gmail.com>
2015-06-15 11:52:26 +0700

    CKEditor

---
+++ DAOLayer/KhoaHoc_NguoiDungDAO.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+using DTOLayer;
+using Data;
+
+namespace DAOLayer
+{
+    public class KhoaHoc_NguoiDungDAO : DAO<KhoaHoc_NguoiDungDAO, KhoaHoc_NguoiDungDTO>
+    {
+        public static KhoaHoc_NguoiDungDTO gan(System.Data.SqlClient.SqlDataReader dong, LienKet lienKet)
+        {
+            KhoaHoc_NguoiDungDTO thanhVien = new KhoaHoc_NguoiDungDTO();
+            
+            int? maTam;
+            for (int i = 0; i < dong.FieldCount; i++)
+            {
+                switch (dong.GetName(i))
+                {
+                    case "NguoiDung":
+                        maTam = layInt(dong, i);
+
+                        if (maTam.HasValue)
+                        {
+                            thanhVien.nguoiDung = LienKet.co(lienKet, "NguoiDung") ?
+                                layDTO<NguoiDungDTO>(NguoiDungDAO.layTheoMa(maTam)) :
+                                new NguoiDungDTO()
+                                {
+                                    ma = maTam
+                                };
+                        }
+                        break;
+                    case "KhoaHoc":
+                        maTam = layInt(dong, i);
+                        
+                        if (maTam.HasValue)
+                        {
+                            thanhVien.khoaHoc = LienKet.co(lienKet, "NguoiDung") ?
+                                layDTO<KhoaHocDTO>(KhoaHocDAO.layTheoMa(maTam)) :
+                                new KhoaHocDTO()
+                                {
+                                    ma = maTam
+                                };
+                        }
+                        break;
+                    case "TrangThai":
+                        thanhVien.trangThai = layInt(dong, i);
+                        break;
+                    case "ThoiDiemThamGia":
+                        thanhVien.thoiDiemThamGia = layDateTime(dong, i);
+                        break;
+                    case "NguoiThem":
+                        maTam = layInt(dong, i);
+
+                        if (maTam.HasValue)
+                        {
+                            thanhVien.nguoiDung = LienKet.co(lienKet, "NguoiThem") ?
+                                layDTO<NguoiDungDTO>(NguoiDungDAO.layTheoMa(maTam)) :
+                                new NguoiDungDTO()
+                                {
+                                    ma = maTam
+                                };
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return thanhVien;
+        }
+    }
+}
---
+++ DTOLayer/KhoaHoc_NguoiDungDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+
+namespace DTOLayer
+{
+    public class KhoaHoc_NguoiDungDTO : DTO
+    {
+        public NguoiDungDTO nguoiDung;
+        public KhoaHocDTO khoaHoc;
+        /// <summary>
+        /// 0: Thành viên ---
+        /// 1: Chờ xác nhận đăng ký ---
+        /// 2: Bị chặn
+        /// </summary>
+        public int? trangThai;
+        public DateTime? thoiDiemThamGia;
+        public NguoiDungDTO nguoiThem;
+    }
+}
