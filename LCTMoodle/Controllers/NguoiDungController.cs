996448cc0166c97e6e9bf0c6b868fa1d0ebd008b
PenguinBlueChip <Funky_Hpio@yahoo.com>
2015-07-17 00:37:03 +0700

    nothing

--- LCTMoodle/Controllers/NguoiDungController.cs
+++ LCTMoodle/Controllers/NguoiDungController.cs
@@ -363,7 +363,7 @@ namespace LCTMoodle.Controllers
         [HttpPost]
         public ActionResult XuLyThemTinNhan(FormCollection form)
         {
-            var ketQua = TinNhanBUS.them(chuyenForm(form), new LienKet() { "NguoiDung" }, Session["NguoiDung"] as int?);
+            var ketQua = TinNhanBUS.them(chuyenForm(form), new LienKet() { "NguoiDung" });
             if (ketQua.trangThai != 0)
             {
                 return Json(ketQua);
