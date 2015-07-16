2712a7b8e9e9383069981f68e9f3d20ccc69ad3d
PenguinBlueChip <Funky_Hpio@yahoo.com>
2015-07-17 00:23:02 +0700

    nothing

--- BUSLayer/TinNhanBUS.cs
+++ BUSLayer/TinNhanBUS.cs
@@ -77,13 +77,13 @@ namespace BUSLayer
             }
         }
 
-        public static KetQua them(Form form, LienKet lienKet = null, int? maNguoiNhan = null)
+        public static KetQua them(Form form, LienKet lienKet = null, int? maNguoiGui = null)
         {
-            if (!maNguoiNhan.HasValue)
+            if (!maNguoiGui.HasValue)
             {
                 return new KetQua(3, "Bạn chưa đăng nhập");
             }
-            form.Add("MaNguoiGui", Session["NguoiDung"].ToString());
+            form.Add("MaNguoiGui", maNguoiGui.ToString());
 
             TinNhanDTO tinNhan = new TinNhanDTO();            
             gan(ref tinNhan, form);
