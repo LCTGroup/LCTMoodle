7eac7db163c2c3b8e49927ce45faa83472372cf5
csphong94 <ng.huyphong94@gmail.com>
2015-07-17 20:27:13 +0700

    hp

---
+++ LCTMoodle/WebServices/wcf_KhoaHoc_DienDan_BinhLuan.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace LCTMoodle.WebServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "wcf_KhoaHoc_DienDan_BinhLuan" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select wcf_KhoaHoc_DienDan_BinhLuan.svc or wcf_KhoaHoc_DienDan_BinhLuan.svc.cs at the Solution Explorer and start debugging.
+    public class wcf_KhoaHoc_DienDan_BinhLuan : Iwcf_KhoaHoc_DienDan_BinhLuan
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
