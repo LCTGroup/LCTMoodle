872972fa673335808d468548c073e1a460117cc6
csphong94 <ng.huyphong94@gmail.com>
2015-07-19 18:37:33 +0700

    hp

--- LCTMoodle/WebServices/Iwcf_CaNhan.cs
+++ LCTMoodle/WebServices/Iwcf_CaNhan.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using LCTMoodle.WebServices.Client_Model;
 
 namespace LCTMoodle.WebServices
 {
@@ -12,6 +13,12 @@ namespace LCTMoodle.WebServices
     public interface Iwcf_CaNhan
     {
         [OperationContract]
-        string cauHoi(int maNguoiDung);
+        List<clientmodel_CaNhan> cauHoi(int maNguoiDung);
+
+        [OperationContract]
+        List<clientmodel_CaNhan> traLoi(int maNguoiDung);
+
+        [OperationContract]
+        List<clientmodel_CaNhan> khoaHoc(int maNguoiDung);
     }
 }
--- LCTMoodle/WebServices/wcf_CaNhan.svc.cs
+++ LCTMoodle/WebServices/wcf_CaNhan.svc.cs
@@ -5,6 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 using LCTMoodle.WebServices.Client_Model;
+using LCTMoodle.LCTView;
 using DTOLayer;
 using BUSLayer;
 
@@ -14,12 +15,105 @@ namespace LCTMoodle.WebServices
     // NOTE: In order to launch WCF Test Client for testing this service, please select wcf_CaNhan.svc or wcf_CaNhan.svc.cs at the Solution Explorer and start debugging.
     public class wcf_CaNhan : Iwcf_CaNhan
     {
-        public string cauHoi(int maNguoiDung)
+        /// <summary>
+        /// Webservice lấy hoạt động câu hỏi
+        /// </summary>
+        /// <param name="maNguoiDung"></param>
+        /// <returns>List<clientmodel_CaNhan> </returns>
+        public List<clientmodel_CaNhan> cauHoi(int maNguoiDung)
         {
-            KetQua ketQuaDS = CauHoiBUS.layCauHoi_DanhSachMaLienQuan(maNguoiDung);
-            KetQua ketQua = HoatDongBUS.lay_CuaDanhSachDoiTuong("CH", ketQuaDS.ketQua as string, 1, 5, new LienKet() { "HanhDong" });
-            List<HoatDongDTO> dto_HoatDong = ketQua.ketQua as List<HoatDongDTO>;
-            return dto_HoatDong[0].hanhDong.loiNhan;
+            KetQua ketQuaDSMa = CauHoiBUS.layCauHoi_DanhSachMaLienQuan(maNguoiDung);
+            List<clientmodel_CaNhan> lst_CaNhan = new List<clientmodel_CaNhan>();
+
+            if(ketQuaDSMa.trangThai == 0)
+            {
+                KetQua ketQua = HoatDongBUS.lay_CuaDanhSachDoiTuong("CH", ketQuaDSMa.ketQua as string, 1, 3, new LienKet { "HanhDong" });
+
+                if (ketQua.trangThai == 0)
+                {
+                    foreach (var cauHoi in ketQua.ketQua as List<HoatDongDTO>)
+                    {
+                        lst_CaNhan.Add(new clientmodel_CaNhan()
+                        {
+                            loai = 0,
+                            noiDung = HoatDongView.text(cauHoi, maNguoiDung),
+                        });
+
+                        if (cauHoi.loaiDoiTuongTacDong == "CH")
+                        {
+                            lst_CaNhan[lst_CaNhan.Count - 1].ma = cauHoi.maDoiTuongTacDong.Value;
+                        }
+
+                        if (cauHoi.loaiDoiTuongBiTacDong == "CH")
+                        {
+                            lst_CaNhan[lst_CaNhan.Count - 1].ma = cauHoi.maDoiTuongBiTacDong.Value;
+                        }
+                    }
+                }
+            }
+
+            return lst_CaNhan;
+        }
+
+        /// <summary>
+        /// Webservice lấy hoạt động trả lời
+        /// </summary>
+        /// <param name="maNguoiDung"></param>
+        /// <returns>List<clientmodel_CaNhan></returns>
+        public List<clientmodel_CaNhan> traLoi(int maNguoiDung)
+        {
+            KetQua ketQuaDSMa = TraLoiBUS.layTraLoi_DanhSachMaLienQuan(maNguoiDung);
+            List<clientmodel_CaNhan> lst_CaNhan = new List<clientmodel_CaNhan>();
+
+            if (ketQuaDSMa.trangThai == 0)
+            {
+                KetQua ketQua = HoatDongBUS.lay_CuaDanhSachDoiTuong("TL", ketQuaDSMa.ketQua as string, 1, 3, new LienKet { "HanhDong" });
+
+                if (ketQua.trangThai == 0)
+                {
+                    foreach (var cauHoi in ketQua.ketQua as List<HoatDongDTO>)
+                    {
+                        lst_CaNhan.Add(new clientmodel_CaNhan()
+                        {
+                            loai = 0,
+                            noiDung = HoatDongView.text(cauHoi, maNguoiDung),
+                        });
+
+                        if (cauHoi.loaiDoiTuongTacDong == "TL")
+                        {
+                            lst_CaNhan[lst_CaNhan.Count - 1].ma = cauHoi.maDoiTuongTacDong.Value;
+                        }
+
+                        if (cauHoi.loaiDoiTuongBiTacDong == "TL")
+                        {
+                            lst_CaNhan[lst_CaNhan.Count - 1].ma = cauHoi.maDoiTuongBiTacDong.Value;
+                        }
+                    }
+                }
+            }
+
+            return lst_CaNhan;
+        }
+
+        /// <summary>
+        /// Websservice lây hành động bài tập
+        /// </summary>
+        /// <param name="maNguoiDung"></param>
+        /// <returns></returns>
+        public List<clientmodel_KhoaHoc_BaiTap> khoaHoc(int maNguoiDung)
+        {
+            KetQua ketQua = BaiVietBaiTapBUS.layDanhSachCanHoanThanh(maNguoiDung);
+            List<clientmodel_KhoaHoc_BaiTap> lst_CaNhan = new List<clientmodel_KhoaHoc_BaiTap>();
+
+            if(ketQua.trangThai == 0)
+            {
+                foreach(var baiTap in ketQua.ketQua as List<KhoaHocDTO>)
+                {
+
+                }
+            }
+
+            return lst_CaNhan;
         }
     }
 }
